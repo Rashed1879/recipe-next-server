847d1ee72b98b965d758e6e6e4c579308c24c297
Rashed1879 <rashedul15-1879@diu.edu.bd>
2024-01-19 21:07:27 +0600

    all api created

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+//checking if server is running
 app.get('/', (req, res) => {
 	res.send('Server IS Running');
 });
@@ -28,25 +29,72 @@ async function run() {
 	try {
 		// Connect the client to the server	(optional starting in v4.7)
 		await client.connect();
+
+		// created the collection for storing recipes
 		const recipesCollection = client
 			.db('recipeNextDB')
 			.collection('recipes');
 
+		// search recipe by title
+		app.get('/search-recipe/:text', async (req, res) => {
+			const searchedName = req.params.text;
+			const result = await recipesCollection
+				.find({
+					title: { $regex: searchedName, $options: 'i' },
+				})
+				.toArray();
+			res.send(result);
+		});
+
+		// inserting recipe into the collection
 		app.post('/recipes', async (req, res) => {
 			const recipe = req.body;
 			const result = await recipesCollection.insertOne(recipe);
 			res.send(result);
 		});
 
+		// getting all the recipes from the collection
 		app.get('/allrecipes', async (req, res) => {
 			const result = await recipesCollection.find().toArray();
 			res.send(result);
 		});
 
+		// finding single recipe according to id from the collection
 		app.get('/recipe/:id', async (req, res) => {
 			const id = req.params.id;
 			const query = { _id: new ObjectId(id) };
 			const result = await recipesCollection.findOne(query);
+			console.log(result);
+			res.send(result);
+		});
+
+		// updating single recipe according to id into the collection
+		app.put('/recipe/:id', async (req, res) => {
+			const id = req.params.id;
+			const recipe = req.body;
+			const query = { _id: new ObjectId(id) };
+			const option = { upsert: true };
+			const updateRecipe = {
+				$set: {
+					title: recipe.updatedTitle,
+					ingredients: recipe.updatedIngredients,
+					instruction: recipe.updatedInstruction,
+					optionalMedia: recipe.updatedOptionalMedia,
+				},
+			};
+			const result = await recipesCollection.updateOne(
+				query,
+				updateRecipe,
+				option
+			);
+			res.send(result);
+		});
+
+		// delete a recipe according to id from the collection
+		app.delete('/recipe/:id', async (req, res) => {
+			const id = req.params.id;
+			const query = { _id: new ObjectId(id) };
+			const result = await recipesCollection.deleteOne(query);
 			res.send(result);
 		});
 
@@ -62,6 +110,7 @@ async function run() {
 }
 run().catch(console.dir);
 
+//listening to port
 app.listen(port, () => {
 	console.log(`Server is running on port, ${port}`);
 });
